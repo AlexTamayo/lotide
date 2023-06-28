ad22c3bdfdd4372fcb8287406cb1933b9a064750
Alex Tamayo <catd00@gmail.com>
2023-06-28 01:05:12 -0700

    This function takes a 'source' and a 'itemsToRemove' arrays and returns a new array with only those elements from source not present in itemsToRemove.

---
+++ without.js
@@ -0,0 +1,57 @@
+const eqArrays = function(array1, array2) {
+  let condition = false;
+  for (let i in array1) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } else {
+      condition = true;
+    }
+  }
+  return condition;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  let condition = false;
+  for (let x of source) {
+    for (let y of itemsToRemove) {
+      if (x !== y) {
+        condition = true;
+      } else {
+        condition = false;
+        break;
+      }
+    }
+    if (condition) {
+      newArray.push(x);
+    }
+  }
+  console.log(newArray);
+  return newArray;
+};
+
+
+if (require.main === module) {
+
+  console.log("This is the main module");
+
+  without([1, 2, 3], [1]); // => [2, 3]
+
+  without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+  
+  const words = ["hello", "world", "lighthouse"];
+  without(words, ["lighthouse"]); // no need to capture return value for this test case
+  // Make sure the original array was not altered by the without function
+  assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+}
+
