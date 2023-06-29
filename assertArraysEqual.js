074f01de5b716ee7cadcb228f7120f7104713f71
Alex Tamayo <catd00@gmail.com>
2023-06-28 18:27:35 -0700

    Changed eqArrays to handle empty arrays as it didn't have that functionality before.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,20 @@
 const eqArrays = function(array1, array2) {
   let condition = false;
-  for (let i in array1) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } else {
-      condition = true;
+  if (array1.length === 0) {
+    if (array2.length === 0) {
+      return condition = true;
     }
+  } else {
+    for (let i in array1) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      } else {
+        condition = true;
+      }
+    }
+    return condition;
+    
   }
-  return condition;
 };
 
 const assertArraysEqual = function(array1, array2) {
@@ -31,4 +38,5 @@ if (require.main === module) {
   
   console.log("\n");
 }
+
 
--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,23 @@
 const eqArrays = function(array1, array2) {
   let condition = false;
-  for (let i in array1) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } else {
-      condition = true;
+  if (array1.length === 0) {
+    if (array2.length === 0) {
+      return condition = true;
     }
+  } else {
+    for (let i in array1) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      } else {
+        condition = true;
+      }
+    }
+    return condition;
+    
   }
-  return condition;
 };
 
-/*
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -18,19 +25,26 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-*/
+
 
 if (require.main === module) {
 
   console.log("This is the main module");
 
   eqArrays([1, 2, 3], [1, 2, 3]); // => true
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
   eqArrays([1, 2, 3], [3, 2, 1]); // => false
+  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
   
   eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-  eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
   
-  // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+  eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+  
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+  assertEqual(eqArrays([], []), true); // => should PASS
 
 }
 
