05c5a568689a16a01e52157e330abf0d94bc98ce
Alex Tamayo <catd00@gmail.com>
2023-06-29 02:21:38 -0700

    implementation of findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,51 @@
+/*
+
+Implement the function findKeyByValue which takes in an object and a value.
+It should scan the object and return the first key which contains the given value.
+If no key with that given value is found, then it should return undefined.
+
+
+INPUT
+  object and value
+
+FLOW
+  check the object's value if value in key match input value then return the key
+
+OUTPUT
+  return key string of a given value.
+
+*/
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+if (require.main === module) {
+
+  const bestTVShowsByGenre = {
+    sciFi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+
+  const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+}
+
